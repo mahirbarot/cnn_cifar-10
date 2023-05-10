ae01444e83eb9172f24f721e19a04fda0aeaf770
Mahir Barot <46795655+mahirbarot@users.noreply.github.com>
2023-05-11 03:06:56 +0530

    Update app.py
    added model integration
    -made website more dynamic
    -is able to predict images on it's own

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import pandas as pd
 import streamlit as st
 import numpy as np
+import tensorflow as tf
+from PIL import Image
 import time
 
-import streamlit as st
+model = tf.keras.models.load_model('cnn_cifar10.h5')
 
 st.title('Image Classifier!')
 st.title(f'Our Project is _trained using_ :blue[CNN model] and utilizes the CIFAR-10 dataset :sunglasses:')
@@ -39,7 +40,28 @@ st.metric("Accuracy", "CNN", "72%")
 st.write("")
 st.write("")
 
-def loader():
+
+uploaded_file = st.file_uploader("Upload your image", type=['jpg','png','jpeg'])
+class_names = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 
+                5: 'dog', 6: 'frog', 7: 'horse', 8: 'ship', 9: 'truck'}
+
+if st.button("Process"):
+    def preprocess_image(image):
+        # Resize the image to the size expected by the model
+        image = image.resize((32, 32))
+        # Convert the image to a numpy array
+        image_array = np.array(image)
+        # Normalize the pixel values
+        image_array = image_array / 255.0
+        # Expand the dimensions to create a batch of size 1
+        image_array = np.expand_dims(image_array, axis=0)
+        return image_array
+
+
+
+    
+    if uploaded_file is not None:
+        # Load the image and preprocess it
         st.write("")
         st.subheader("Processing inputs...")
         progress=st.progress(0)
@@ -47,8 +69,23 @@ def loader():
             time.sleep(0.03)
             progress.progress(i+1)
         st.balloons()
-        st.warning("Feature will be added soon!")
+        
+        image = Image.open(uploaded_file)
+        cpy=image
+        x = preprocess_image(image)
+        # Use the model to make a prediction
+        y = model.predict(x)
+        # Get the predicted class label
+        class_label = np.argmax(y)
+        cpy = cpy.resize((120, 120))
+        st.image(cpy)
+        # Show the predicted class label to the user
+        st.write("The predicted class label is:", class_names[class_label])
+        st.subheader(class_names[class_label])
 
-st.file_uploader("Upload image to classfiy...")
-st.button("Process",key='bt1',on_click=loader)
+
+
+
+
+        
 
